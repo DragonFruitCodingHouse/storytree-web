11ebc93aaad967d60512123dc14d60fc33c3b861
Viniel Kumar <vkumar@v1021-wn-27-199.campus-dynamic.uwaterloo.ca>
2017-09-16 03:44:24 -0700

    bootstrap and jquery cdn

--- src/footer.js
+++ src/footer.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
-import './App.css';
 
 class Footer extends Component {
   render() {
     return (
       <div className="Footer">
-      	<h3> &copy; 2017 DragonFruit CodingHouse. All Rights Reserved. </h3>
+      	<h3>&copy; 2017 DragonFruit CodingHouse. All Rights Reserved.</h3>
       </div>
     );
   }
