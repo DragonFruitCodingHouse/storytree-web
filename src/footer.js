f78f3a57172f12d7cca773ec4316eb7aec22df69
Viniel Kumar <vkumar@v1021-wn-27-199.campus-dynamic.uwaterloo.ca>
2017-09-16 03:21:19 -0700

    added bootstrap files and updated navbar and footer

--- src/footer.js
+++ src/footer.js
@@ -4,7 +4,7 @@ class Footer extends Component {
   render() {
     return (
       <div className="Footer">
-      	<h1> &copy; 2017 DragonFruit CodingHouse. All Rights Reserved. </h1>
+      	<h3> &copy; 2017 DragonFruit CodingHouse. All Rights Reserved. </h3>
       </div>
     );
   }
--- src/navbar.js
+++ src/navbar.js
@@ -4,8 +4,31 @@ class Navbar extends Component {
   render() {
     return (
       <div className="Navbar">
-      	<h1> Header </h1>
-      	
+        <nav class="navbar navbar-light bg-faded">
+          <a class="navbar-brand" href="#">
+            <img src="" width="30" height="30" class="d-inline-block align-top" alt=""/>
+            Story Tree
+          </a>
+
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item active">
+                <a class="nav-link" href="#">Browse</a>
+              </li>
+            </ul>
+          </div>
+
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item active">
+                <a class="nav-link" href="#">Login</a>
+              </li>
+              <li class="nav-item active">
+                <a class="nav-link" href="#">Sign Up</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
       </div>
     );
   }
