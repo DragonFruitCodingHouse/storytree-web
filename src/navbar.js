24faefc057c35f7e320e459e6297ea7616c1b7bf
Viniel Kumar <vkumar@v1021-wn-27-199.campus-dynamic.uwaterloo.ca>
2017-09-16 05:14:19 -0700

    updated navbar

--- src/navbar.js
+++ src/navbar.js
@@ -4,27 +4,34 @@ class Navbar extends Component {
   render() {
     return (
       <div className="Navbar">
-        <nav class="navbar navbar-light bg-faded">
-          <a class="navbar-brand" href="#">
-            <img src="" width="30" height="30" class="d-inline-block align-top" alt=""/>
+        <nav className="navbar navbar-toggleable-md navbar-light bg-faded">
+          <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <a className="navbar-brand" href="#">
+            <img src="" width="30" height="30" alt=""/>
             Story Tree
           </a>
 
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-              <li class="nav-item active">
-                <a class="nav-link" href="#">Browse</a>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item active">
+                <a className="nav-link" href="#">Browse <span className="sr-only">(current)</span></a>
               </li>
             </ul>
           </div>
 
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-              <li class="nav-item active">
-                <a class="nav-link" href="#">Login</a>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item active">
+                <a className="nav-link" href="#">Browse <span className="sr-only">(current)</span></a>
               </li>
-              <li class="nav-item active">
-                <a class="nav-link" href="#">Sign Up</a>
+              <li className="nav-item active">
+                <a className="nav-link" href="#">Login</a>
+              </li>
+              <li className="nav-item active">
+                <a className="nav-link" href="#">Sign Up</a>
               </li>
             </ul>
           </div>
