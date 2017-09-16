db1a60ea5f6f1c22c6a7ef516497daa0a9648b53
Pritpal Chauhan <pchauhan@Pritpals-MacBook-Pro.local>
2017-09-16 06:24:50 -0400

    Updated app.js

--- src/App.js
+++ src/App.js
@@ -11,6 +11,27 @@ class App extends Component {
     return (
       <div className="App">
       	<Navbar/>
+      		<div className="container">
+      			<div className="row">
+      				<div className="col-12">
+      					<h1> Title </h1>
+      					<h3> By Dfruit </h3>
+      				
+      					<button type="button" className="sect btn btn-outline-primary dropdown-toggle"> Section 1 </button>
+      					<button type="button" className="sort btn btn-outline-primary dropdown-toggle"> Sort By </button>
+      					<button type="button" className="fullscreen btn btn-outline-primary"> Full Screen </button>
+      					<button type="button" className="save btn btn-outline-primary"> Save </button>
+      					<button type="button" className="undo btn btn-outline-primary"> Undo </button>
+      					<button type="button" className="add btn btn-outline-primary"> Add </button>
+      					
+      					<div className="firstCard">
+      						<p> This is the start of a very cool story. </p>
+      					</div>
+      					
+      					<div className="row"> </div>
+      				</div>
+      			</div>
+      		</div>
       	<Footer/>
       </div>
     );
