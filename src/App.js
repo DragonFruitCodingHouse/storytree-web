797b5f43e22fd331d979c47a80372e7f0cab5db7
Pritpal Chauhan <pchauhan@Pritpals-MBP.lan>
2017-09-16 09:11:31 -0400

    more app.js changes

--- src/App.js
+++ src/App.js
@@ -14,37 +14,40 @@ class App extends Component {
       		<div className="container w-100 p-0">
       			<div className="row w-100 m-0">
       				<div className="col-12 p-0">
-      					<h1 className="w-75 m-auto"> Title </h1>
-      					<h3 className="text-right w-25 m-auto"> By Dfruit </h3>
+      					<h1 className="w-75 m-auto pt-5"> Title </h1>
+      					<h4 className="text-right w-25 m-auto pt-1"> By: Dfruit </h4>
       				
-	  					<div className="dropdown d-inline-block">
-	      					<button type="button" className="sect btn btn-outline-primary dropdown-toggle d-inline-block" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Section 1 </button>
-	      					<div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-		  					  	<a className="dropdown-item" href="#lol">Section 1</a>
-	    						<a className="dropdown-item" href="#lol">Section 2</a>
-	   							<a className="dropdown-item" href="#lol">Section 3</a>
+	  					<div className="bg-inverse text-white clearfix mt-5 p-2">
+		  					<div className="dropdown d-inline-block float-left">
+		      					<button type="button" className="sect btn btn-outline-primary dropdown-toggle d-inline-block" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Section 1 </button>
+		      					<div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+			  					  	<a className="dropdown-item" href="#lol">Section 1</a>
+		    						<a className="dropdown-item" href="#lol">Section 2</a>
+		   							<a className="dropdown-item" href="#lol">Section 3</a>
+								</div>
 							</div>
-						</div>
-						<button type="button" className="fullscreen btn btn-outline-primary d-inline-block"> Full Screen </button>
-						
-						<div className="dropdown d-inline-block">
-	      					<button type="button" className="sort btn btn-outline-primary dropdown-toggle d-inline-block" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Sort By </button>
-	      					<div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-		  					  	<a className="dropdown-item" href="#lol">Highest Score</a>
-	    						<a className="dropdown-item" href="#lol">Lowest Score</a>
-	   							<a className="dropdown-item" href="#lol">Newest</a>
-	   							<a className="dropdown-item" href="#lol">Oldest</a>
+							<button type="button" className="fullscreen btn btn-outline-primary d-inline-block float-left"> Full Screen </button>
+							
+							<div className="dropdown d-inline-block">
+		      					<button type="button" className="sort btn btn-outline-primary dropdown-toggle d-inline-block" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Sort By </button>
+		      					<div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+			  					  	<a className="dropdown-item" href="#lol">Highest Score</a>
+		    						<a className="dropdown-item" href="#lol">Lowest Score</a>
+		   							<a className="dropdown-item" href="#lol">Newest</a>
+		   							<a className="dropdown-item" href="#lol">Oldest</a>
+								</div>
 							</div>
-						</div>
-						<button type="button" className="undo btn btn-outline-primary d-inline-block"> Undo </button>
-						<button type="button" className="x btn btn-outline-primary d-inline-block"> X </button>
-						
-						<div className="firstCard">
-      						<p> This is the start of a very cool story. </p>
+							
+							<button type="button" className="x btn btn-outline-primary d-inline-block float-right"> X </button>
+							<button type="button" className="undo btn btn-outline-primary d-inline-block float-right"> Undo </button>
+							
+							<div className="firstCard">
+	      						<p> This is the start of a very cool story. </p>
+	      					</div>
+							
+	      					<button type="button" className="save btn btn-outline-primary d-inline-block float-left"> Save </button>
+	      					<button type="button" className="add btn btn-outline-primary d-inline-block float-right"> Add </button>
       					</div>
-						
-      					<button type="button" className="save btn btn-outline-primary d-inline-block"> Save </button>
-      					<button type="button" className="add btn btn-outline-primary d-inline-block"> Add </button>
       					
       					<div className="bg-faded w-100">
 	      					<div className="carousel slide w-75 m-auto" data-ride="carousel" data-interval="false" id="carouselExampleControls"> 
