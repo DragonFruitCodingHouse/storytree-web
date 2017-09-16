b803bcb0ecf0dd2d40570b2cf5a1273bd67b1801
Pritpal Chauhan <pchauhan@Pritpals-MacBook-Pro.local>
2017-09-16 05:32:27 -0400

    Created footer & navbar

---
+++ src/footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+  render() {
+    return (
+      <div className="Footer">
+      	<h1> &copy; 2017 DragonFruit CodingHouse. All Rights Reserved. </h1>
+      </div>
+    );
+  }
+}
+
+export default Footer;
