46cb6d558bcf77ac80b2ae9b61b99b1a54a05ffb
Viniel Kumar <vkumar@v1021-wn-27-199.campus-dynamic.uwaterloo.ca>
2017-09-16 11:22:10 -0700

    added routing + fixed bugs

--- src/App.js
+++ src/App.js
@@ -82,7 +82,6 @@ class Book extends Component {
       		<div className="container w-100 p-0">
       			<div className="row w-100 m-0">
       				<div className="col-12 p-0">
-<<<<<<< HEAD
       					<h1 className="w-75 m-auto"> Title </h1>
       					<h3 className="text-right w-25 m-auto"> By Dfruit </h3>
 
@@ -117,45 +116,6 @@ class Book extends Component {
 
       					<div className="bg-faded w-100">
 	      					<div className="carousel slide w-75 m-auto" data-ride="carousel" data-interval="false" id="carouselExampleControls">
-=======
-      					<h1 className="w-75 m-auto pt-5"> Title </h1>
-      					<h4 className="text-right w-25 m-auto pt-1"> By: Dfruit </h4>
-      				
-	  					<div className="bg-inverse text-white clearfix mt-5 p-2">
-		  					<div className="dropdown d-inline-block float-left">
-		      					<button type="button" className="sect btn btn-outline-primary dropdown-toggle d-inline-block" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Section 1 </button>
-		      					<div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-			  					  	<a className="dropdown-item" href="#lol">Section 1</a>
-		    						<a className="dropdown-item" href="#lol">Section 2</a>
-		   							<a className="dropdown-item" href="#lol">Section 3</a>
-								</div>
-							</div>
-							<button type="button" className="fullscreen btn btn-outline-primary d-inline-block float-left"> Full Screen </button>
-							
-							<div className="dropdown d-inline-block">
-		      					<button type="button" className="sort btn btn-outline-primary dropdown-toggle d-inline-block" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Sort By </button>
-		      					<div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-			  					  	<a className="dropdown-item" href="#lol">Highest Score</a>
-		    						<a className="dropdown-item" href="#lol">Lowest Score</a>
-		   							<a className="dropdown-item" href="#lol">Newest</a>
-		   							<a className="dropdown-item" href="#lol">Oldest</a>
-								</div>
-							</div>
-							
-							<button type="button" className="x btn btn-outline-primary d-inline-block float-right"> X </button>
-							<button type="button" className="undo btn btn-outline-primary d-inline-block float-right"> Undo </button>
-							
-							<div className="firstCard">
-	      						<p className="p-5"> This is the start of a very cool story. </p>
-	      					</div>
-							
-	      					<button type="button" className="save btn btn-outline-primary d-inline-block float-left"> Save </button>
-	      					<button type="button" className="add btn btn-outline-primary d-inline-block float-right"> Add </button>
-      					</div>
-      					
-      					<div className="bg-faded w-100 p-5">
-	      					<div className="carousel slide w-75 m-auto" data-ride="carousel" data-interval="false" id="carouselExampleControls"> 
->>>>>>> 9ebe2bf8977ed752e8f228e32237254c80265280
 	      						<div className="carousel-inner w-100" role="listbox">
 		      						<div className="carousel-item active w-100 m-auto">
 		      							<div className="w-75 mr-2 p-3">
